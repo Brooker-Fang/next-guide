8c840ab4fab4b970e257ca4038a1643c25d83928
comedyOfKing <f_hhong@163.com>
2021-02-14 16:12:29 +0800

    commit

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router'
 export default function Post({data}) {
+  const router = useRouter()
+  // 获取router的isFallback状态
+  // true表示正在生成静态页面
+  if (router.isFallback) {
+    return <div>loading</div>
+  }
   return <div>
     <span>{data.id}</span>
     <span>{data.title}</span>
@@ -10,19 +17,24 @@ export async function getStaticPaths () {
     // 返回固定格式的路由参数
     paths: [{params: {id: '1'}}, {params: {id: '2'}}],
     // 当用户访问的路由参数没有在当前函数中返回时，是否显示404页面
-    fallback: false
+    // false 则跳转404页面, true则会通过参数重新生成静态页面
+    fallback: true
   }
 }
 // 返回路由参数所对应的具体的数据
 export async function getStaticProps ({params}) {
-  const id = params.id
+  const id = params.id + ''
   let data;
+  console.log('hello', id, typeof id)
   switch(id) {
-    case 1: 
+    case '1': 
       data = {id: 1, title: 'post1'}
       break
-    case 2: 
+    case '2': 
       data = {id: 21, title: 'post21'}
+      break
+    case '3':
+      data = {id: 3, title: 'fallback true'}
       break
     default: 
       data = {}
