4c567d71483d050b0697c79d332af4c5232b3292
comedyOfKing <f_hhong@163.com>
2021-02-17 21:51:02 +0800

    commit

---
+++ pages/google/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GooglePage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default GooglePage
